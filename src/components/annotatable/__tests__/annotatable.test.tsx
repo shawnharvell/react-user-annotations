bf7dc641ef90c1dcb49c85ff582e67808b464c97
Shawn Harvell <sharvell@jbecker.com>
2021-07-21 09:07:46 -0400

    feat: adding markers to annotatable functional

--- src/components/annotatable/__tests__/annotatable.test.tsx
+++ src/components/annotatable/__tests__/annotatable.test.tsx
@@ -1,12 +1,105 @@
 import React from "react";
-import { Annotatable, AnnotatableProps } from "..";
-import { render, screen } from "@testing-library/react";
+import { Annotatable, AnnotatableProps, enterAnnotationMode } from "..";
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import * as useMouse from "@react-hook/mouse-position";
+
+jest.spyOn(useMouse, "default").mockReturnValue({
+  x: 100,
+  y: 100,
+  elementWidth: 1000,
+  elementHeight: 1000,
+  pageX: 0,
+  pageY: 0,
+  clientX: 0,
+  clientY: 0,
+  screenX: 0,
+  screenY: 0,
+  isDown: false,
+  isOver: false,
+  isTouch: false,
+});
 
 const defaultProps: AnnotatableProps = { persistenceKey: "unit-test-key" };
 
 describe("Annotatable", () => {
   it("renders", () => {
     render(<Annotatable {...defaultProps}>my child content</Annotatable>);
-    expect(screen.getByText("my child content"));
+    expect(screen.getByText("my child content")).toBeTruthy();
+  });
+
+  it("alt-CLICK adds pins", () => {
+    render(<Annotatable {...defaultProps}>my child content</Annotatable>);
+    const container = screen.getByText("my child content");
+    expect(container).toBeTruthy();
+
+    expect(screen.queryAllByTestId("note-marker").length).toEqual(0);
+    act(() => {
+      userEvent.hover(container);
+    });
+    act(() => {
+      userEvent.click(container, { altKey: true });
+    });
+    expect(screen.queryAllByTestId("note-marker").length).toEqual(1);
+  });
+
+  it("regular CLICK + add mode adds pins (and without doesn't)", async () => {
+    jest.useFakeTimers();
+
+    const { container: renderContainer } = render(
+      <Annotatable {...defaultProps}>my child content</Annotatable>
+    );
+    const container = screen.getByText("my child content");
+    expect(container).toBeTruthy();
+
+    expect(screen.queryAllByTestId("note-marker").length).toEqual(0);
+    act(() => {
+      userEvent.click(container);
+    });
+    expect(screen.queryAllByTestId("note-marker").length).toEqual(0);
+
+    act(() => {
+      enterAnnotationMode();
+    });
+    expect(renderContainer.querySelectorAll(".mode-flash").length).toEqual(1);
+    act(() => {
+      jest.advanceTimersByTime(700);
+    });
+    expect(renderContainer.querySelectorAll(".mode-flash").length).toEqual(0);
+
+    act(() => {
+      userEvent.click(container);
+    });
+    expect(screen.queryAllByTestId("note-marker").length).toEqual(1);
+
+    jest.useRealTimers();
+  });
+
+  it("providing an intial array of pins works as expected", () => {
+    const initialPins = [
+      { xPixels: 100, yPixels: 100, xPercent: 10, yPercent: 10, color: "red", guid: "123" },
+      { xPixels: 200, yPixels: 200, xPercent: 20, yPercent: 20, color: "red", guid: "456" },
+      { xPixels: 300, yPixels: 300, xPercent: 30, yPercent: 30, color: "red", guid: "789" },
+    ];
+    render(
+      <Annotatable initialPins={initialPins} positionTechnique="percent" initialMode="add">
+        my child content
+      </Annotatable>
+    );
+    expect(screen.getByText("my child content")).toBeTruthy();
+    expect(screen.queryAllByTestId("note-marker").length).toEqual(3);
+  });
+
+  it("when mouse position is unavailable, pin add attempt is no-op", () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    jest.spyOn(useMouse, "default").mockReturnValueOnce(undefined);
+    render(<Annotatable>my child content</Annotatable>);
+    expect(screen.getByText("my child content")).toBeTruthy();
+
+    act(() => {
+      userEvent.click(screen.getByText("my child content"), { altKey: true });
+    });
+    expect(screen.queryAllByTestId("note-marker").length).toEqual(0);
   });
 });
--- src/components/annotatable/annotatable.tsx
+++ src/components/annotatable/annotatable.tsx
@@ -1,9 +1,139 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classnames from "classnames";
+import useMouse from "@react-hook/mouse-position";
+import { v4 as uuidv4 } from "uuid";
+
+export type AnnotatableMode = "add" | "view";
+export type PositionTechnique = "pixels" | "percent";
+
+export interface PinProps {
+  xPercent: number;
+  yPercent: number;
+  xPixels: number;
+  yPixels: number;
+  color: string;
+  guid: string;
+}
+
+export interface PinStyle {
+  background?: string;
+  borderRadius?: string;
+  width?: string;
+  height?: string;
+  marginTop?: string;
+  marginLeft?: string;
+}
+
+export const defaultPinStyle: PinStyle = {
+  background: "red",
+  borderRadius: "10px",
+  width: "20px",
+  height: "20px",
+  marginTop: "-10px",
+  marginLeft: "-10px",
+};
 
 export interface AnnotatableProps {
   persistenceKey?: string;
+  positionTechnique?: PositionTechnique;
+  initialMode?: AnnotatableMode;
+  initialPins?: PinProps[];
 }
 
-export const Annotatable: React.FC<AnnotatableProps> = ({ children, persistenceKey = "" }) => {
-  return <div data-react-user-annotations-persistence-key={persistenceKey}>{children}</div>;
+export const Annotatable: React.FC<AnnotatableProps> = ({
+  children,
+  persistenceKey = uuidv4(),
+  positionTechnique = "pixels",
+  initialMode = "view",
+  initialPins = [],
+}) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  const [pins, setPins] = useState<PinProps[]>(initialPins);
+  const [mode, setMode] = useState<AnnotatableMode>(initialMode);
+  const [showFlash, setShowFlash] = useState<boolean>(false);
+
+  const mouse = useMouse(ref, {
+    enterDelay: 100,
+    leaveDelay: 100,
+  });
+
+  const onClick: React.MouseEventHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (e.altKey || mode === "add") {
+      // alt-click OR a standard click when in add mode will create a new pin/note
+      if (mouse && mouse.x && mouse.elementWidth && mouse.y && mouse.elementHeight) {
+        const newPin = {
+          xPercent: (mouse.x / mouse.elementWidth) * 100,
+          yPercent: (mouse.y / mouse.elementHeight) * 100,
+          xPixels: mouse.x,
+          yPixels: mouse.y,
+          color: "red",
+          guid: uuidv4(),
+        };
+        setPins((prev) => [...prev, newPin]);
+      }
+      setMode("view");
+    }
+  };
+
+  useEffect(() => {
+    const invokeAddMode = () => {
+      setMode("add");
+    };
+    document.addEventListener("react-user-annotations-invoke-add", invokeAddMode);
+    return () => {
+      document.removeEventListener("react-user-annotations-invoke-add", invokeAddMode);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (mode === "add") {
+      setShowFlash(true);
+      setTimeout(() => {
+        setShowFlash(false);
+      }, 500);
+    }
+  }, [mode]);
+
+  return (
+    <div
+      ref={ref}
+      onClick={onClick}
+      data-react-user-annotations-persistence-key={persistenceKey}
+      className={classnames("react-user-annotations-annotatable", { "mode-flash": showFlash })}
+    >
+      {children}
+      {pins.map((pin) => {
+        const style =
+          positionTechnique === "pixels"
+            ? {
+                ...defaultPinStyle,
+                left: `${pin.xPixels.toFixed(2)}px`,
+                top: `${pin.yPixels.toFixed(2)}px`,
+                background: pin.color,
+              }
+            : {
+                ...defaultPinStyle,
+                left: `${pin.xPercent.toFixed(2)}%`,
+                top: `${pin.yPercent.toFixed(2)}%`,
+                background: pin.color,
+              };
+        return (
+          <div
+            style={style}
+            key={pin.guid}
+            className="react-user-annotations-marker"
+            data-testid="note-marker"
+          >
+            &nbsp;
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const enterAnnotationMode = (): void => {
+  document.dispatchEvent(new CustomEvent("react-user-annotations-invoke-add"));
 };
